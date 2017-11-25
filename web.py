3a35421aadfaceb66a420a5760ead262af7628c8
Akifev95 <31894933+Akifev95@users.noreply.github.com>
2017-11-25 20:13:00 +0500

    Create web.py

---
+++ web.py
@@ -0,0 +1,38 @@
+from jinja import Environment, FileSystemLoader
+from webob import Request
+import os
+
+assets = [
+	'app.js',
+	'react.js',
+	'leaflet.js',
+	'D3.js',
+	'moment.js',
+	'math.js',
+	'main.css',
+	'bootstrap.css',
+	'normalize.css',
+	]
+js = []
+css = []
+
+
+def app(environ, start_response):
+	response_code = '200 OK'
+	response_type = ('Content-Type', 'text/HTML')
+	start_response(response_code, [response_type])
+
+	for item in assets:
+		separator = item.split('.')
+		if separator[1] == 'js':
+			js.append(item)
+		elif separator[1] == 'css':
+			css.append(item)
+			
+
+	env = Environment(loader=FileSystemLoader('.'))
+	template = env.get_template('index.html')
+	print(template.render(css0=css, js0=js))
+
+req2 = Request.blank('index.html')
+print(req2.get_response(app))
